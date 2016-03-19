24f0f2435bd4b0fbbf251ea6f44cf9f70d591bfd
Khaffel <khaffel@gmail.com>
2016-03-19 12:27:58 +0100

    Base interaction thinks

--- src/com/wire2D/operations/Interaction.java
+++ src/com/wire2D/operations/Interaction.java
@@ -1,14 +1,11 @@
 package com.wire2D.operations;
 
-import com.wire2D.object.Box;
 import com.wire2D.utis.Window;
 import org.lwjgl.input.Mouse;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.state.StateBasedGame;
-
-import java.util.ArrayList;
 
 /**
  * Created by Mazek on 2016-03-16.
@@ -28,14 +25,12 @@ public class Interaction {
         g.drawString(String.valueOf(posY), 10, 65);
     }
 
-    public void update(GameContainer gc, StateBasedGame s, int i, ArrayList<Box> Box) throws SlickException {
+    public void update(GameContainer gc, StateBasedGame s, int i) throws SlickException {
         posX = Mouse.getX();
         posY = Window.HEIGHT - Mouse.getY();
 
         if (Mouse.isButtonDown(0)) {
-            for(Box box : Box){
 
-            }
         }
     }
 }
