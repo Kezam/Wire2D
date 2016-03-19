afc0f242d564f9638425c381c6b8b649bc7b83f0
Mazek27 <mazekpl@gmail.com>
2016-03-19 10:17:47 +0100

    Update GUI

--- src/com/wire2D/GUI/Life_bar.java
+++ src/com/wire2D/GUI/Life_bar.java
@@ -8,35 +8,39 @@ import org.newdawn.slick.SlickException;
 import org.newdawn.slick.geom.Rectangle;
 import org.newdawn.slick.state.StateBasedGame;
 
+
 /**
  * Created by Mazek on 2016-03-18.
  */
+class Life_bar {
 
-public class Life_bar {
-
-    private float health;
+    private float aHealth;
+    private float cHealth;
     private Rectangle lifeBar;
-    private Rectangle lifeBar_frame;
+    private Rectangle lifeBar_freame;
 
-    public Life_bar(float health, float maxHealth){
-        this.health = health;
-        this.lifeBar_frame = new Rectangle(20, 660, 200, 20);
-        this.lifeBar = new Rectangle(20, 660, 2 * health, 20);
+    Life_bar(float health, float maxHealth){
+        this.aHealth = health;
+        this.cHealth = maxHealth;
+        this.lifeBar_freame = new Rectangle(20,660,200,20);
+        this.lifeBar = new Rectangle(20,660,2 * health,20);
     }
 
 
     public void render(GameContainer gc, StateBasedGame sbg, Graphics g) throws SlickException {
-        //g.drawString();
         g.setColor(Color.red);
         g.fill(lifeBar);
         g.draw(lifeBar);
 
         g.setColor(Color.white);
-        g.draw(lifeBar_frame);
+        g.draw(lifeBar_freame);
+        g.drawString("HP: " + aHealth + "/" + cHealth, 20,660);
     }
 
     public void update(GameContainer gc, StateBasedGame s, int i, Statistics stats) throws SlickException {
-        this.health = stats.aHealth;
-        lifeBar.setWidth(2 * health);
+        this.aHealth = stats.aHealth;
+        this.cHealth = stats.maxHealth;
+        lifeBar.setWidth(200*(aHealth/cHealth));
     }
+
 }
